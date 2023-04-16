293e71063dc635504657742f7f65b3ec5632da68
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-04-17 04:18:03 +0900

    00_04_ログを出そう

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 
+#include <string>
+#include <format>
+
 
 //ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
@@ -18,6 +21,22 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 
 	//標準のめっせーっじ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+
+
+//string  -> wstring
+std::wstring ConvertString(const std::string& str);
+
+
+//wstring -> string
+std::string ConvertString(const std::wstring& str);
+
+
+
+/* ---出力ウィンドウに文字を出す--- */
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
 }
 
 
@@ -88,6 +107,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	MSG msg{};
 
+
+
 	///// -----メインループ----- /////
 	//ウィンドウの×ボタンが押されるまでループ
 	while (msg.message != WM_QUIT) {
@@ -107,4 +128,48 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 	return 0;
+}
+
+
+
+
+
+
+
+/* -----プロトタイプ宣言----- */
+
+//string  -> wstring
+std::wstring ConvertString(const std::string& str)
+{
+	if (str.empty())
+	{
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0)
+	{
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+//wstring -> string
+std::string ConvertString(const std::wstring& str)
+{
+	if (str.empty())
+	{
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0)
+	{
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
 }
