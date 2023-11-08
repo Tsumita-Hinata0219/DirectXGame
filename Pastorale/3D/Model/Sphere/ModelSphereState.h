0dbeaefd603aebad35b2ee1bb4f875f00c9c7eda
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-11-08 11:03:03 +0900

    ModelSphereStateクラスを追加

--- Pastorale/3D/Model/Sphere/ModelSphereState.h
+++ Pastorale/3D/Model/Sphere/ModelSphereState.h
@@ -1,1 +1,53 @@
 #pragma once
+#include "Function.h"
+#include "Struct.h"
+#include "WorldTransform.h"
+#include "TextureManager.h"
+#include "DirectXCommon.h"
+#include "NormalGraphicPipeline.h"
+#include "CreateResource.h"
+#include "IModelState.h"
+
+
+
+/* ModelPlaneクラス */
+class ModelSphereState : public IModelState {
+
+public: // メンバ関数
+
+	ModelSphereState() {};
+	~ModelSphereState() {};
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Initialize(Model* pModel) override;
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw(Model* pModel, WorldTransform worldTransform, Matrix4x4& viewMatrix) override;
+
+
+private: // メンバ関数
+
+	/// <summary>
+	/// コマンドコール処理
+	/// </summary>
+	void CommandCall(uint32_t texture);
+
+
+private: // メンバ関数
+
+	// リソース
+	ResourcePeroperty resource_{};
+
+	// 半径
+	float radius_ = 1.0f;
+
+	// 光データ
+	DirectionalLight light_;
+
+	// 分割数
+	int32_t subdivision_ = 16;
+};
