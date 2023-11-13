4f85f127d66217583a9b393d74cdc0d9a91c4a39
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-11-13 17:05:42 +0900

    ok

---
+++ Pastorale/3D/Model/ModelObj/ModelObjState.h
@@ -0,0 +1,63 @@
+#pragma once
+
+
+#include "Function.h"
+#include "Struct.h"
+#include "WorldTransform.h"
+#include "TextureManager.h"
+#include "DirectXCommon.h"
+#include "NormalGraphicPipeline.h"
+#include "LightGraphicPipeline.h"
+#include "CreateResource.h"
+#include "IModelState.h"
+#include <fstream>
+#include <sstream>
+
+
+
+/* ModelObjクラス */
+class ModelObjState : public IModelState {
+
+public: // メンバ関数
+
+	ModelObjState() {};
+	~ModelObjState() {};
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Initialize(Model* pModel) override;
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw(Model* pModel, WorldTransform worldTransform, Matrix4x4& viewMatrix) override;
+
+
+private: // メンバ関数
+
+	/// <summary>
+	/// コマンドコール処理
+	/// </summary>
+	void CommandCall(uint32_t texture);
+
+	/// <summary>
+	/// objのファイルを読み込む
+	/// </summary>
+	ModelData LoadObjFile(Model* pModel, const std::string& directoryPath);
+
+	/// <summary>
+	/// mtlファイルを読み込む関数
+	/// </summary>
+	MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename);
+
+
+private: // メンバ関数
+
+	// リソース
+	ResourcePeroperty resource_{};
+
+	// ModelData
+	ModelData modelData_{};
+
+};
