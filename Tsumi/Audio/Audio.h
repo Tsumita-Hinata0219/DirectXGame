1e51bbfae01e5e14710cbdf2fb9ccdbbeb84e258
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-11-29 12:03:44 +0900

    Audioクラス追加

--- Tsumi/Audio/Audio.h
+++ Tsumi/Audio/Audio.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include <xaudio2.h>
+
+#pragma comment(lib, "xaudio2.lib")
+
+#include <fstream>
+
+#include "Struct.h"
+
+
+
+/* Audioクラス */
+class Audio {
+
+public: // メンバ関数
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void BeginFrame();
+
+
+private: // メンバ関数
+
+
+
+private: // メンバ変数
+
+	ComPtr<IXAudio2> xAudio2_ = nullptr;
+	IXAudio2MasteringVoice* masterVoice_;
+
+};
