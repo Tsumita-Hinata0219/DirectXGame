999f80defc5fb7c583faa9ec74be09f14f799e44
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-12-18 15:04:23 +0900

    スプライトがうまくいかん

--- Resources/shaders/SpriteObject3d.VS.hlsl
+++ Resources/shaders/SpriteObject3d.VS.hlsl
@@ -23,8 +23,10 @@ VertexShaderOutput main(VertexShaderInput input) {
 	VertexShaderOutput output;
 	float32_t4x4 resultMat;
 
-	float32_t4x4 cameraMat = mul(gTransformationViewMatrix.view, gTransformationViewMatrix.viewProjection);
-	resultMat = mul(gTransformationMatrix.WVP, cameraMat);
+	//float32_t4x4 cameraMat = mul(gTransformationViewMatrix.view, gTransformationViewMatrix.viewProjection);
+	//resultMat = mul(gTransformationMatrix.WVP, cameraMat);
+
+	resultMat = mul(gTransformationMatrix.WVP, gTransformationViewMatrix.orthoGraphic); 
 
 	output.position = mul(input.position, resultMat);
 	output.texcoord = input.texcoord;
